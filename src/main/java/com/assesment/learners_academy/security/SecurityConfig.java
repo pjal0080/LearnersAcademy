6648b32dd5cf7106f94231e2277dc26a87ce60cd
Piyush Pranjal (pipranja) <pipranja@cisco.com>
2021-09-17 16:13:39 +0530

    password encoder added successfully

--- src/main/java/com/assesment/learners_academy/security/SecurityConfig.java
+++ src/main/java/com/assesment/learners_academy/security/SecurityConfig.java
@@ -1,16 +1,28 @@
 package com.assesment.learners_academy.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
 
     @Override
@@ -18,8 +30,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         auth
                 .inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
                 .withUser("admin")
-                .password("{noop}admin")
+                .password(passwordEncoder.encode("admin"))
                 .roles("ADMIN");
 
 
